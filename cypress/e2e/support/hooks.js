63e776330dd63dda8700413794a92e071b4be367
Erol <122645226+erolisldk@users.noreply.github.com>
2024-07-11 02:12:47 +0300

    7/11/2024 1:53

--- cypress/e2e/support/hooks.js
+++ cypress/e2e/support/hooks.js
@@ -1,28 +1,9 @@
 const { Before, After } = require('@badeball/cypress-cucumber-preprocessor');
 
 Before(() => {
-  if (Cypress.isBrowser('!firefox')) cy.setCookie('banner', 'true');
-  cy.wrap([]).as('roomIds');
-  cy.wrap([]).as('roomNames');
-  cy.wrap(false).as('roomsCreated');
+
 });
 
 After(() => {
-  cy.get('@roomsCreated').then((roomsCreated) => {
-    if (roomsCreated) {
-      cy.log('Deleting Rooms...');
-      cy.get('@roomIds').then((roomIds) => {
-        roomIds.forEach((roomId) => {
-          cy.deleteRoomViaApi(roomId);
-        });
-      });
-      cy.get('@roomNames').then((roomNames) => {
-        roomNames.forEach((roomName) => {
-          cy.deleteAllRoomsViaApi(roomName);
-        });
-      });
-    } else {
-      cy.log('There are NO Rooms Created. Skipping...');
-    }
-  });
+ 
 });
